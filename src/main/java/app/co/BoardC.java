55758f4b795bafa39746da2452521935633bcbb8
DAEHYUNKIM <zcvfcat@gmail.com>
2019-08-13 16:19:34 +0900

    v0.0.4

--- src/main/java/app/co/BoardC.java
+++ src/main/java/app/co/BoardC.java
@@ -1,13 +1,21 @@
 package app.co;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import app.mo.BoardM;
 import app.repo.BoardR;
@@ -36,15 +44,21 @@ public class BoardC {
     
     @GetMapping("/")
     public String list(Model model) {
-        List<BoardM> list = boardSe.findAll();
-		model.addAttribute("list", list);
-		return "list";
-    }
-
-    @GetMapping("/{page}")
-    public String page(Model model) {
+        Pageable pageable = PageRequest.of(0, 10, new Sort(Direction.DESC, "idx"));
+        Page<BoardM> page = boardRepo.findAll(pageable);
+        model.addAttribute("page", page);
         return "list";
     }
+
+    @GetMapping("/{index}")
+    public String page(@PathVariable int index, Model model) {
+        Pageable pageable = PageRequest.of(index-1, 10, new Sort(Direction.DESC, "idx"));
+        Page<BoardM> page = boardRepo.findAll(pageable);
+        model.addAttribute("page", page);
+        return "list";
+    }
+
+
 
     @GetMapping("/read/{idx}")
     public String read(@PathVariable int idx, Model model){
@@ -70,5 +84,21 @@ public class BoardC {
     public String delete(BoardM boardDTO) {
         boardSe.delete(boardDTO);
         return "redirect:/";
+    }
+
+    @GetMapping("/test/page")
+    public String page1(Model model) {
+        Pageable pageable = PageRequest.of(0, 10, new Sort(Direction.DESC, "idx"));
+        Page<BoardM> page = boardRepo.findAll(pageable);
+        model.addAttribute("page", page);
+        return "lists";
+    }
+
+    @ResponseBody
+    @GetMapping("/test/")
+    public Map<String,Object> test(Model model) {
+        List<BoardM> list = boardSe.findAll();
+		model.addAttribute("list", list);
+        return model.asMap();
     }
 }
