b2b1b68a69ad829a7d26a8f42d06b341d6d75530
DAEHYUNKIM <zcvfcat@gmail.com>
2019-08-14 16:45:14 +0900

    v2.1.0

--- src/main/java/app/co/BoardA.java
+++ src/main/java/app/co/BoardA.java
@@ -10,7 +10,12 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +25,7 @@ import app.repo.BoardR;
 import app.se.BoardS;
 
 @RestController
+@CrossOrigin("origin-allowed=*")
 @RequestMapping(value="/board")
 public class BoardA {
 
@@ -29,39 +35,39 @@ public class BoardA {
 	@Autowired
     BoardR br;
     
-    @RequestMapping(value={"","/","/{index}"}, method=RequestMethod.GET)
+    @GetMapping(value={"","/","/{index}"})
     public Page<BoardM> list(@PathVariable Optional<Integer> index) {
         Pageable pageable = PageRequest.of(index.orElse(1)-1,10,new Sort(Direction.DESC,"idx"));
         Page<BoardM> page = br.findAll(pageable);
         return page;
     }
 
-    @RequestMapping(value={"/read/{index}"}, method=RequestMethod.GET)
+    @GetMapping(value={"/read/{index}"})
     public BoardM read(@PathVariable int index){
         BoardM bm = bs.read(index);
         return bm;
     }
 
-    @RequestMapping(value={"/insert"},method=RequestMethod.POST)
-    public Map<String,String> insert(BoardM bm){
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public Map<String,String> insert(@RequestBody BoardM bm){
         Map<String,String> map = new HashMap<String,String>();
         bs.create(bm);
         map.put("message", "success");
         return map;
     }
-    
-    @RequestMapping(value={"/update"},method=RequestMethod.PUT)
-    public Map<String,String> update(BoardM bm){
+
+    @RequestMapping(value = "/", method = RequestMethod.PUT)
+    public Map<String,String> update(@RequestBody BoardM bm){
         Map<String,String> map = new HashMap<String,String>();
-        String ms = bs.update(bm)?"fail":"success";
+        String ms = bs.update(bm)?"success":"fail";
         map.put("message",ms);
         return map;
     }
 
-    @RequestMapping(value={"/delete"},method=RequestMethod.DELETE)
-    public Map<String,String> delete(BoardM bm){
+    @RequestMapping(value={"/"},method=RequestMethod.DELETE)
+    public Map<String,String> delete(@RequestBody BoardM bm){
         Map<String,String> map = new HashMap<String,String>();
-        String ms = bs.delete(bm)?"fail":"success";
+        String ms = bs.delete(bm)?"success":"fail";
         map.put("message",ms);
         return map;
     }
