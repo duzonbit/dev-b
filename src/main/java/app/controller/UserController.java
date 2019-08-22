6c5122893c8c71acb7a0647c4e0e7c60c2645d91
DAEHYUNKIM <zcvfcat@gmail.com>
2019-08-22 20:32:05 +0900

    t

--- src/main/java/app/controller/UserController.java
+++ src/main/java/app/controller/UserController.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import app.model.UserModel;
-import app.repository.UserRepo;
 import app.service.UserService;
 
 /**
